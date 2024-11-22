3a05bac0b9d60033ebf452a6b774cbb73252cccf
Matthew <matthew@Matthews-Laptop.local>
2024-11-22 16:59:16 +0300

    Added arc_length, sector_area, segment_area to circle.py

--- circle.py
+++ circle.py
@@ -8,3 +8,15 @@ def area(r):
 def perimeter(r):
     return 2 * math.pi * r
 
+
+def arc_length(r, angle):
+    return (angle / 360) * perimeter(r)
+
+
+def sector_area(r, angle):
+    return (angle / 360) * area(r)
+
+
+def segment_area(r, angle):
+    angle_rad = math.radians(angle)
+    return (r * r / 2) * (angle_rad - math.sin(angle_rad))
