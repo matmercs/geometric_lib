9305da916bd10b1b7b17f9afb3c07c6b6db3cc1b
Matthew <matthew@Matthews-Laptop.local>
2024-11-22 17:07:38 +0300

    Added semi_perimeter, heron_area, is_valid_triangle, circumscribed_circle_radius and inscribed_circle_radius  to triangle.py

--- triangle.py
+++ triangle.py
@@ -4,3 +4,25 @@ def area(a, h):
 
 def perimeter(a, b, c):
     return a + b + c
+
+
+def semi_perimeter(a, b, c):
+    return perimeter(a, b, c) / 2
+
+
+def heron_area(a, b, c):
+    s = semi_perimeter(a, b, c)
+    return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
+
+def is_valid_triangle(a, b, c):
+    return a + b > c and a + c > b and b + c > a
+
+
+def circumscribed_circle_radius(a, b, c):
+    return (a * b * c) / (4 * heron_area(a, b, c))
+
+
+def inscribed_circle_radius(a, b, c):
+    p = semi_perimeter(a, b, c)
+    return heron_area(a, b, c) / p
